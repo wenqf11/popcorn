d798c5e75ac39ebe9b84180c5393d36d9862468b
yl-1993 <yl93528@gmail.com>
2015-02-14 21:58:50 +0800

    add admin

--- end/devicemgt/forms.py
+++ end/devicemgt/forms.py
@@ -4,6 +4,8 @@ __author__ = 'LY'
 from django import forms
 
 #定义表单模型
+
+#用户表单
 class UserForm(forms.Form):
     username = forms.CharField(label='用户名：',max_length=100)
     password = forms.CharField(label='密码：',widget=forms.PasswordInput())
--- end/devicemgt/views.py
+++ end/devicemgt/views.py
@@ -2,8 +2,9 @@ __author__ = 'LY'
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render,render_to_response
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
+from django.contrib.auth.models import User
 from models import k_user
 from forms import *
 
@@ -17,7 +18,27 @@ def index(request):
     else:
         return HttpResponseRedirect('/login/')
 
-
+#注册,
+def register(req):
+    if req.method == 'POST':
+        uf = UserForm(req.POST)
+        if uf.is_valid():
+            #获得表单数据
+            username = uf.cleaned_data['username']
+            password = uf.cleaned_data['password']
+            #添加到数据库
+            user = k_user.objects.create(username= username,password=password, classid_id=1, permission_id = 1)
+            olduser = User.objects.create_user(
+                username=user.username,
+                email=user.email,
+                password=user.password
+            )
+            user.save()
+            olduser.save()
+            return HttpResponse('register success!!')
+    else:
+        uf = UserForm()
+    return render_to_response('register.html',{'uf':uf}, context_instance=RequestContext(req))
 
 #处理登录请求
 def login(request):
