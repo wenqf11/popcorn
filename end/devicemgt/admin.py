3b8b6a1e94909c34a0b31a6f41fb8e98414ce607
yl-1993 <yl93528@gmail.com>
2015-02-20 17:05:22 +0800

    add dashboard

---
+++ end/devicemgt/admin.py
@@ -0,0 +1,6 @@
+__author__ = 'LY'
+
+from django.contrib import admin
+from models import k_user
+
+admin.site.register(k_user)
--- end/devicemgt/views.py
+++ end/devicemgt/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render,render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login
 from models import k_user
 from forms import *
 
@@ -12,13 +13,73 @@ from forms import *
 def index(request):
     if request.user.is_authenticated():
         #登陆成功
+        #user=k_user.objects.get(username=request.user.username)
+        user=User.objects.get(username=request.user.username)
         #读取权限，显示内容
-        variables=RequestContext(request,{'test':'hello'})
+        variables=RequestContext(request,{'username':user.username, 'clicked_item': 'index'})
         return render_to_response('index.html',variables)
     else:
         return HttpResponseRedirect('/login/')
 
-#注册,
+#用户管理
+def usermgt(request):
+    if request.user.is_authenticated():
+        #登陆成功
+        #user=k_user.objects.get(username=request.user.username)
+        user=User.objects.get(username=request.user.username)
+        #读取权限，显示内容
+        variables=RequestContext(request,{'username':user.username, 'clicked_item': 'user'})
+        return render_to_response('index.html',variables)
+    else:
+        return HttpResponseRedirect('/login/')
+
+#设备管理
+def devicemgt(request):
+    if request.user.is_authenticated():
+        #登陆成功
+        #user=k_user.objects.get(username=request.user.username)
+        user=User.objects.get(username=request.user.username)
+        #读取权限，显示内容
+        variables=RequestContext(request,{'username':user.username, 'clicked_item': 'device'})
+        return render_to_response('index.html',variables)
+    else:
+        return HttpResponseRedirect('/login/')
+
+#个人信息
+def profile(request):
+    if request.user.is_authenticated():
+        #登陆成功
+        #user=k_user.objects.get(username=request.user.username)
+        user=User.objects.get(username=request.user.username)
+        #读取权限，显示内容
+        variables=RequestContext(request,{'username':user.username})
+        return render_to_response('index.html',variables)
+    else:
+        return HttpResponseRedirect('/login/')
+
+def front(request):
+    if request.user.is_authenticated():
+        #登陆成功
+        #user=k_user.objects.get(username=request.user.username)
+        user=User.objects.get(username=request.user.username)
+        #读取权限，显示内容
+        variables=RequestContext(request,{'username':user.username, 'clicked_item': 'front'})
+        return render_to_response('index.html',variables)
+    else:
+        return HttpResponseRedirect('/login/')
+
+def setting(request):
+    if request.user.is_authenticated():
+        #登陆成功
+        #user=k_user.objects.get(username=request.user.username)
+        user=User.objects.get(username=request.user.username)
+        #读取权限，显示内容
+        variables=RequestContext(request,{'username':user.username, 'clicked_item': 'setting'})
+        return render_to_response('index.html',variables)
+    else:
+        return HttpResponseRedirect('/login/')
+
+#注册, 创建一个新用户
 def register(req):
     if req.method == 'POST':
         uf = UserForm(req.POST)
@@ -49,8 +110,10 @@ def login(request):
             username = uf.cleaned_data['username']
             password = uf.cleaned_data['password']
             #获取的表单数据与数据库进行比较
-            user = k_user.objects.filter(username__exact = username,password__exact = password)
-            if user:
+            #user = k_user.objects.filter(username = username,password = password)
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
                 return render_to_response('index.html',{'username':username})
             else:
                 return HttpResponseRedirect('/login/')
