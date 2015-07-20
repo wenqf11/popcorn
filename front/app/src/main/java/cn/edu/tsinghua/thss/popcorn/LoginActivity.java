af10d9fbba09a6e6ddf64f46dea3fbcad3fbf15f
wenqingfu <thssvince@163.com>
2015-07-20 22:54:08 +0800

    read Token from local cache

--- front/app/src/main/java/cn/edu/tsinghua/thss/popcorn/LoginActivity.java
+++ front/app/src/main/java/cn/edu/tsinghua/thss/popcorn/LoginActivity.java
@@ -175,7 +175,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
     }
 
     private boolean isPasswordValid(String password) {
-        return password.length() > 5;
+        return password.length() > 2;
     }
 
     /**
