99eee7fdbdeba25005e00fcfe7ed99e6b877e50a
wenqf11 <thssvince@163.com>
2015-08-18 01:46:39 +0800

    delete south module

--- end/devicemgt/settings.py
+++ end/devicemgt/settings.py
@@ -136,8 +136,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'devicemgt',
-    'south'
+    'devicemgt'
 )
 
 # A sample logging configuration. The only tangible logging
