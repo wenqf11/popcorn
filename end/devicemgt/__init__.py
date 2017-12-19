a7a589a258a703669e17a3fa3d125bb9e4dc6fc1
HUANG Huaiyi <hhyysbg@163.com>
2017-12-20 00:48:30 +0800

    device plan

--- end/devicemgt/__init__.py
+++ end/devicemgt/__init__.py
@@ -8,66 +8,68 @@ __All__ = ["InitThread"]
 class InitThread:
 
     def __init__(self):
-        schedule = sched.scheduler(time.time, time.sleep)
+        self.schedule = sched.scheduler(time.time, time.sleep)
         nowtime = time.localtime(time.time())
-        remainedsec = 86400-(nowtime.tm_hour*3600+nowtime.tm_min*60+nowtime.tm_sec)-60
+        remainedsec = 4#86400-(nowtime.tm_hour*3600+nowtime.tm_min*60+nowtime.tm_sec)-60
         thread = threading.Thread(target = self.thread_fun, args = (remainedsec,))
         thread.start()
 
     def get_endday(self, st, prd):
-    	if prd == "day":
-    		return st + timedelta(days=1)
-    	elif prd == "week":
-    		return st + timedelta(days=7)
-    	elif prd == "halfmonth":
-    		return st + timedelta(days=15)
-    	elif prd == "month":
-    		return st + timedelta(days=30)
-    	elif prd == "twomonth":
-    		return st + timedelta(days=61)
-    	elif prd == "threemonth":
-    		return st + timedelta(days=91)
-    	elif prd == "fourmonth":
-    		return st + timedelta(days=121)
-    	elif prd == "halfyear":
-    		return st + timedelta(days=182)
-    	elif prd == "year":
-    		return st + timedelta(days=365)
-    	elif prd == "twoyear":
-    		return st + timedelta(days=730)
+        if prd == "day":
+            return st + timedelta(days=1)
+        elif prd == "week":
+            return st + timedelta(days=7)
+        elif prd == "halfmonth":
+            return st + timedelta(days=15)
+        elif prd == "month":
+            return st + timedelta(days=30)
+        elif prd == "twomonth":
+            return st + timedelta(days=61)
+        elif prd == "threemonth":
+            return st + timedelta(days=91)
+        elif prd == "fourmonth":
+            return st + timedelta(days=121)
+        elif prd == "halfyear":
+            return st + timedelta(days=182)
+        elif prd == "year":
+            return st + timedelta(days=365)
+        elif prd == "twoyear":
+            return st + timedelta(days=730)
 
     def perform_command(self, cmd, inc):
         _deviceplans = k_deviceplan.objects.all()
         _today = date.today()
         for _dp in _deviceplans:
-        	_maintenance = k_maintenance.objects.get(id=_dp.maintenanceid_id)
-        	_startday = _maintenance.assigndatetime.date()
-        	_endday = get_endday(_startday, _dp.period)
-        	if _today < _endday:
-        		if _maintenance.state == '4' or _maintenance.state == '5':
-        			_newmaintenance = k_maintenance.objects.create(mtype=1,classid=_maintenance.classid,deviceid_id=_maintenance.deviceid_id,state=2)
-        			_newmaintenance.creatorid = _maintenance.creatorid
-        			_newmaintenance.assignorid = _maintenance.assignorid
-        			_newmaintenance.assigndatetime = get_current_time()
-        			_newmaintenance.title = _maintenance.title
-        			_newmaintenance.createcontent = _maintenance.createcontent
-        			_newmaintenance.editorid = _maintenance.editorid
-        			_newmaintenance.memo = _maintenance.memo
-        			_newmaintenance.state = 2
-        			_newmaintenance.save()
-        			_dp.maintenanceid_id = _newmaintenance.id
-        			_dp.save()
+            _maintenance = k_maintenance.objects.get(id=_dp.maintenanceid_id)
+            _startday = _maintenance.assigndatetime.date()
+            _endday = self.get_endday(_startday, _dp.period)
+            if _today < _endday:
+                if _maintenance.state == '4' or _maintenance.state == '5':
+                    _newmaintenance = k_maintenance.objects.create(mtype=1,classid=_maintenance.classid,deviceid_id=_maintenance.deviceid_id,state=2)
+                    _newmaintenance.creatorid = _maintenance.creatorid
+                    _newmaintenance.assignorid = _maintenance.assignorid
+                    _newmaintenance.assigndatetime = get_current_time()
+                    _newmaintenance.title = _maintenance.title
+                    _newmaintenance.createcontent = _maintenance.createcontent
+                    _newmaintenance.editorid = _maintenance.editorid
+                    _newmaintenance.memo = _maintenance.memo
+                    _newmaintenance.state = 2
+                    _newmaintenance.save()
+                    _dp.maintenanceid_id = _newmaintenance.id
+                    _dp.save()
             else:
                 if _maintenance.state != '4' and _maintenance.state != '5':
                     _maintenance.createdatetime = get_current_time()
                     _maintenance.assigndatetime = get_current_time()
                     _maintenance.save()
-        schedule.enter(inc, 0, self.perform_command, (cmd, inc))
-        schedule.run()
-            
+        self.schedule.enter(inc, 0, self.perform_command, (cmd, inc))
+        self.schedule.run()
+
     def timming_exe(self, cmd, inc):
-        schedule.enter(inc, 0, self.perform_command, (cmd, 86400))
-        schedule.run()
+        self.schedule.enter(inc, 0, self.perform_command, (cmd, 3))
+        self.schedule.run()
 
     def thread_fun(self, remainedsec):
         self.timming_exe("123", remainedsec)
+
+InitThread()
--- end/manage.py
+++ end/manage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-from __init__ import InitThread
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devicemgt.settings")
@@ -9,7 +8,3 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-
-    print 123
-
-    InitThread()
